201321435eb28bdc1abe8959f3648fdff5e7ea84
aimr-colab <masuk.aimr@gmail.com>
2026-06-08 01:04:26 +0700

    Update pages.js

--- konten/pages.js
+++ konten/pages.js
@@ -1,6 +1,7 @@
 // konten/pages.js
 
 import { home } from "./pages/home.js";
+import { home } from "./pages/robopragma.js";
 import { tentangKami } from "./pages/tentang-kami.js";
 import { kontak } from "./pages/kontak.js";
 
@@ -17,6 +18,7 @@ export const SITE = {
 
 export const PAGES = {
   "/": home,
+  "/roboprgama": home,
   "/tentang-kami": tentangKami,
   "/kontak": kontak
 };
