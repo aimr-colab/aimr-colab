f02a4bd5cefa54c1b48ac5c64fde04a59fd6b38c
aimr-colab <masuk.aimr@gmail.com>
2026-06-08 01:20:33 +0700

    Update pages.js

--- konten/pages.js
+++ konten/pages.js
@@ -5,15 +5,15 @@ import { robopragma } from "./pages/robopragma.js";
 import { tentangKami } from "./pages/tentang-kami.js";
 import { kontak } from "./pages/kontak.js";
 
-export const PAGES = {
-  title: page.title,
-  desc: page.desc,
-  afflink: page.afflink,
-  site_name: page.site_name,
-  canonical: page.canonical,
-  logoimage: page.logoimage,
-  image: page.image,
-  favicon: page.favicon
+export const SITE = {
+  title: home.title,
+  desc: home.desc,
+  afflink: home.afflink,
+  site_name: home.site_name,
+  canonical: home.canonical,
+  logoimage: home.logoimage,
+  image: home.image,
+  favicon: home.favicon
 };
 
 export const PAGES = {
